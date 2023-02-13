425c6f80d389fa9de698ac95698f9dd9bf5ff8bc
Gustavo Oliveira <87765086+Gu-Oliveira@users.noreply.github.com>
2023-02-13 20:27:35 -0300

    Atualização de funções
    Foram adicionadas as seguintes melhorias:
    1 - ID específico para cada tarefa;
    2 - Botão para remover a tarefa;
    3 - Melhorias visuais;
    4 - Botão para direcionar para impressão.

--- script.js
+++ script.js
@@ -4,28 +4,56 @@ let listaTarefa = document.querySelector('.tarefas-adicionadas ul')
 
 let contador = document.querySelector('.contador')
 
+let idTarefa = 1
+
 btnAdd.addEventListener('click', addTarefa)
 
 function addTarefa () {
     let tarefa = inputTarefa.value;
 
     if(tarefa){
-        let novaTarefa = document.createElement('li');   
-        novaTarefa.innerHTML = `<input type="checkbox"> ${tarefa}`;
+        let novaTarefa = document.createElement('li');  
+        novaTarefa.id = `tarefa-${idTarefa}`
+        novaTarefa.innerHTML =
+         `<input type="checkbox">
+         <div class="container-tarefa">
+         <div class="tarefa">${tarefa}</div> <div class="remover">X</div>
+         </div>`;
 
         novaTarefa.querySelector("input[type=checkbox]").addEventListener("click",function(){
             this.parentNode.classList.toggle("tarefaConcluida")
         });
 
+        novaTarefa.querySelector(".remover").addEventListener("click", (e)=>{
+            let el = e.target
+            el.closest("li").remove();
+        });
+
          listaTarefa.append(novaTarefa);
+
+         idTarefa++;
+         
          inputTarefa.value = "";
     } else {
-        alert('Adiciona uma tarefa.')
+        alert('Adicione uma tarefa.')
     }
 }
 
-inputTarefa.addEventListener("keydown", (e)=>{
+console.log(listaTarefa.length)
+
+//adiconar tarefa com a tecla enter
+
+inputTarefa.addEventListener("keyup", (e)=>{
     if(e.key === "Enter"){
         addTarefa();
     }
 });
+
+//direcionar pagina para impressao
+
+let btnImp = document.querySelector('.imprimir')
+
+btnImp.addEventListener('click', ()=>{
+    window.print();
+})
+
